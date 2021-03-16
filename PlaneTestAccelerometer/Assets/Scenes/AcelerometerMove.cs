443c9475277b0275a110640cdd08c541f2a1ce28
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2021-03-16 11:24:52 -0600

    Add Acceleromenter app

---
+++ PlaneTestAccelerometer/Assets/Scenes/AcelerometerMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcelerometerMove : MonoBehaviour
+{
+                        //THIS IS THE PLANE SCRIPT
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	//To acces to acelerometer inputs
+
+        float x = Input.acceleration.x; //We acces to x aceleration
+        float y = Input.acceleration.y; //We acces to y aceleration
+
+        //To move an object along an axis we use a function called:
+        //transform.Translate("Xvalue", "Yvalue", "Zvalue") 
+
+        //If we want to move the object along the X axis:
+        //transform.Translate(XVlaue, y, z);
+
+        //The object will move along X axis (positive and
+        //negative) depending on the aceleration input.
+
+        /*Time.deltaTime -- Use this funtion to run the game in any 
+        computer without worrig about the execution velocity. However,
+        we can write all the instructions inside this function:
+
+        FixedUpdate()
+        {
+            //instructions
+        }
+
+        FixedUpdate funtion works as Time.deltaTime function, but we do not
+        write it many times as Time.deltaTime.
+        */
+
+        transform.Translate(x*Time.deltaTime*20,0,0);   //Move the object along the X axis depending on the accelerometer readings
+        transform.Translate(0,0,y*Time.deltaTime*20);   //Move the object along the Z axis depending on The accelerometer readings
+
+        /*
+        We are going to rotate the object around Z
+        //axis, depending on the aceleration input:
+        */
+        //Rotation respect to the Z axis
+        transform.Rotate(0,0,-x);
+        //Rotation respect to the X axis
+        transform.Rotate(y,0,0);
+    }
+}
+
---
+++ PlaneTestAccelerometer/Assets/Scenes/Hello_world.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Hello_world -- is the name of our script
+//This is class
+public class Hello_world : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Start function
+    void Start()
+    {
+        //print("Hello world!");
+    }
+
+    // Update is called once per frame
+    //Update funtion
+    void Update()
+    {
+    	//We print every frame of the game
+    	//we print many  Hello world
+        //print("Hello world!");
+    }
+}
